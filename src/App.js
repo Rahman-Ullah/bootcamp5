8b8154a167ad6161357c1c17ad902ba439b44405
Rahman-Ullah <66229705+Rahman-Ullah@users.noreply.github.com>
2020-06-15 06:40:16 +0100

    after removing some warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Parent from './Parent';
 import CounterContext from './CounterContext';
-import Child from './Child';
 function App() {
   let myvalue = useState(0);
   return (
--- src/Parent.js
+++ src/Parent.js
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Child from './Child';
-import CounterContext from './CounterContext';
 import Chile2 from './Child2';
 
 const Parent = () =>{
-    let Gvalue = useContext(CounterContext);
+
     return(
         <div>
            <Child  />
            <Chile2 />
         </div>
-    );
+    )
 }
 export default Parent;
